d3bef2ab5429ef7c580e5ff225a80cbf41e09c82
hunterw455 <hunter.wilkinson@trojans.dsu.edu>
2024-01-28 10:02:11 -0600

    Adding a menu for the program.

--- csc455-assignment1-pt2/Program.cs
+++ csc455-assignment1-pt2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Assignment 1 - Program performs 4 different features based on the user's inpt.
+
+//  Displays menu options
+using System;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Choose a feature you'd like this program to run by entering a number between 1 and 4.");
+Console.WriteLine("     1 - Random Positive Integer Between 1 And 10");
+Console.WriteLine("     2 - Output Today's Date In Short Date String");
+Console.WriteLine("     3 - Output A Random Dinosaur Name");
+Console.WriteLine("     4 - Perform A Random Action On A String");
