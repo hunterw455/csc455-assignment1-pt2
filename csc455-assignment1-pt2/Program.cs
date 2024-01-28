aa952acb94c29a9af3c223fda967101b504fc5c5
hunterw455 <hunter.wilkinson@trojans.dsu.edu>
2024-01-28 10:08:02 -0600

    Creating if statements to contain features of the program

--- csc455-assignment1-pt2/Program.cs
+++ csc455-assignment1-pt2/Program.cs
@@ -9,3 +9,24 @@ Console.WriteLine("     1 - Random Positive Integer Between 1 And 10");
 Console.WriteLine("     2 - Output Today's Date In Short Date String");
 Console.WriteLine("     3 - Output A Random Dinosaur Name");
 Console.WriteLine("     4 - Perform A Random Action On A String");
+
+string userInput = Console.ReadLine();
+
+//  If statements to contain features of the program
+if(userInput == "1")
+{
+
+}else if(userInput == "2")
+{
+
+}else if(userInput == "3")
+{
+
+}else if(userInput == "4")
+{
+
+}
+else
+{
+    Console.WriteLine("Input a valid integer number between 1 and 4.");
+}
